ba973c28f2d71b0d29a6272597a780d8fc84e1b3
ItxSamin <samin8a@gmail.com>
2025-08-07 11:40:03 +0500

    Added the basic structure of our BlockChain

---
+++ Blockchain.py
@@ -0,0 +1,67 @@
+import hashlib
+import json
+from time import time
+
+
+class BLockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        #Create the genesis block
+        self.new_block(previous_hash = 1 , proof = 100)
+    
+
+
+    def new_Block(self,proof,previous_hash = None):
+        """
+        Creates a new block in the block chain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof':proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        #Reset the current list of transactions
+        self.current_transacions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self,sender,recipient,amount):
+        #Creates a new transaction and adds it to the list of current transactions
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the sender
+        :param recipient: <str> Address of the recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+
+        """
+
+        self.current_transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        return self.chain[-1]
+
+
+    @staticmethod
+    def hash(block):
+        #Hashes a block
+        block_string = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BlockChain
+
 Making my very own crypto
+
+Dependecies:
+libraries
+1: Flask
+2: wonderfull requests
+
+you would need "CURL"
+Atleast Python 3.6 with pip
